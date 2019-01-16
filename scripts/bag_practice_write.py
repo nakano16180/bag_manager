5fe3ebc7d9ce5e54db4607826e95165f3b62dbea
nakano16180 <nakano16180@gmail.com>
2019-01-16 19:00:33 +0900

    rosbagの仕様確認

--- scripts/bag_practice_write.py
+++ scripts/bag_practice_write.py
@@ -1,15 +1,71 @@
+# encoding: utf8
 import rosbag
 from std_msgs.msg import String
 
 import time
 
-bag = rosbag.Bag("../data/test.bag", 'w')
+# 以下の処理でtest.bagに書き込まれるのは最後のhello3
+def bag_write1():
+    bag = rosbag.Bag("../data/test.bag", 'w')
+    print bag.filename
+    try:
+        str = String()
+        str.data = "hello"
+        print "write: ", str.data
 
-try:
-    str = String()
-    str.data = "hello"
+        bag.write("chatter", str)
+        time.sleep(2)
+    finally:
+        bag.close()
 
-    bag.write("chatter", str)
-    time.sleep(2)
-finally:
-    bag.close()
+    bag = rosbag.Bag("../data/test.bag", 'w')
+    try:
+        str = String()
+        str.data = "hello2"
+        print "write: ", str.data
+
+        bag.write("chatter", str)
+        time.sleep(2)
+    finally:
+        bag.close()
+
+    bag = rosbag.Bag("../data/test.bag", 'w')
+    try:
+        str = String()
+        str.data = "hello3"
+        print "write: ", str.data
+
+        bag.write("chatter", str)
+        time.sleep(2)
+    finally:
+        bag.close()
+
+####################################################
+# 以下の処理ならhello1からhello3まで書き込まれる
+def bag_write2():
+    bag = rosbag.Bag("../data/test1.bag", 'w')
+    print bag.filename
+    try:
+        str = String()
+        str.data = "hello1"
+        print "write: ", str.data
+
+        bag.write("chatter", str)
+        time.sleep(2)
+
+        str.data = "hello2"
+        print "write: ", str.data
+
+        bag.write("chatter", str)
+        time.sleep(2)
+
+        str.data = "hello3"
+        print "write: ", str.data
+
+        bag.write("chatter", str)
+        time.sleep(2)
+    finally:
+        bag.close()
+
+#bag_write1()
+bag_write2()
